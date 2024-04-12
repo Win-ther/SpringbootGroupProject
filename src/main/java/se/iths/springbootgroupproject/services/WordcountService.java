a64b1e73009d759762214b7ab43b5a727b0674a8
Win-ther <emil.winther@hotmail.com>
2024-04-12 15:13:40 +0200

    Add api-key to environment variable

--- src/main/java/se/iths/springbootgroupproject/services/WordcountService.java
+++ src/main/java/se/iths/springbootgroupproject/services/WordcountService.java
@@ -18,9 +18,10 @@ public class WordcountService {
     @Retryable
     public String[] analyseText(String text){
         String url = String.format("https://wordcount2.p.rapidapi.com/?text=%s", text);
+        final String WORDCOUNT_APIKEY_ENV = System.getenv("WORDCOUNT_APIKEY");
         return Objects.requireNonNull(restClient.get()
                         .uri(url)
-                        .header("X-RapidAPI-Key", "87306660eamshda4409a1ab43323p1532cbjsn3e85c9bcd44a")
+                        .header("X-RapidAPI-Key", WORDCOUNT_APIKEY_ENV)
                         .header("X-RapidAPI-Host", "wordcount2.p.rapidapi.com")
                         .accept()
                         .retrieve()
